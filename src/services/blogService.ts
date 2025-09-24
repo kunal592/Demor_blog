50a3f01d97a22dbf887584417c52b9d546fdc2dc
kunaldaharwal.h <kunaldaharwal.h@gmail.com>
2025-09-24 07:27:59 +0000

    e1finalf

--- src/services/blogService.ts
+++ src/services/blogService.ts
@@ -1,4 +1,4 @@
-import { apiClient } from './apiClient';
+import apiClient from './apiClient';
 import { Blog, BlogFormData, BlogFilters, ApiResponse, PaginationData, UserInteractions } from '../types';
 
 class BlogService {
